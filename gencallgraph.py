c0afb701d731746840d78d733387b02f123ce660
Jonathan Cook <joncook@nmsu.edu>
2022-01-31 11:07:38 -0700

    added binary mode, multi-profile mode

--- gencallgraph.py
+++ gencallgraph.py
@@ -3,37 +3,73 @@
 #
 # Generate call graph data from gprof call graph table
 # and flat profile
+#
 # Usage: gencallgraph.py <gprof-text-output-filename>
 
 #
-# This script calculates a call graph from the table that gprof
-# outputs
+# This script calculates a call graph from the tables that gprof
+# outputs, along with data on each function
 #
 # TODO: optional output to JSON format
 # TODO: keep global structure of edges, too (maybe?)
 
-import re;
-import sys;
-import os;
-import glob;
+import re
+import sys
+import os
+import glob
+import argparse
+
+debug = False
+doDot = False
 
 #
 # Top level object for a call graph
+# reason: we will extend to read in multiple profiles (from intervals),
+#         so each one will be represented by a CallGraph object
 #
 class CallGraph(object):
+   callgraphID = 1
+   # constructor
    def __init__(self):
       # place to hold flat profile data until nodes are set up
       self.flatProfileData = {}
       # keep table of all objects, index is function ID (# assigned by gprof)
       # "<spontaneous>" is ID 0
       self.nodeTable = {}
+      # keep edges, but what is edge ID?
+      self.edgeTable = {}
+      self.id = CallGraph.callgraphID
+      CallGraph.callgraphID = CallGraph.callgraphID + 1
+   # Output a Dot-formatted representation of the profiled call graph
+   def outputDot(self):
+      print("digraph {")
+      for nid in self.nodeTable:
+         node = self.nodeTable[nid]
+         print('{0} [label="{0}\\n{1}"]'.format(node.name, 
+               node.selfTime+node.totTime))
+      for eid in self.edgeTable:
+         edge = self.edgeTable[eid]
+         print('{0}->{1} [label="{2}"]'.format(edge.caller.name,
+               edge.callee.name, edge.numCalls))
+      print("}")
+   #
+   # Output graph info in one libsvm-formatted data line
+   # TODO: Add depth-limiting options
+   def outputLibSVMLine(self):
+      # <label> <feature-id>:<feature-value> <feature-id>:<feature-value>
+      print("{0}".format(self.id), end="")
+      for nid in self.nodeTable:
+         node = self.nodeTable[nid]
+         print(' {0}:{1}'.format(node.id,
+               node.selfTime+node.totTime), end="")
+      print("")      
 
 #
-# Node in call graph is a function
+# Node: a node in call graph, represents a function
 # - keeps stats, and lists of edges to callers and callees
 # 
 class Node(object):
-   nodeTable = {}
+   # constructor
    def __init__(self,cg,name,fid,totTimePct,selfTime,totTime,numCalls):
       self.cg = cg  # call graph that we are part of
       self.name = name
@@ -48,7 +84,7 @@ class Node(object):
       if fid in cg.nodeTable:
          print("Error: function with ID {0} already exists".format(fid))
       cg.nodeTable[fid] = self
-   # update function if it already has an object
+   # update function data if it already has an object
    def update(self,totTimePct,selfTime,totTime,numCalls):
       self.totTimePct = totTimePct
       self.selfTime = selfTime
@@ -56,11 +92,12 @@ class Node(object):
       self.numCalls = numCalls
    # update data from flat profile
    def updateFlatData(self,flatData):
-      print("Update from flat data")
+      if debug: print("Update from flat data")
    # print info of this function
    def printMe(self):
       print("Node: {0} {1}".format(self.id, self.name))
-      print("  stats: tot%:{0} self:{1} tot:{2} calls:{3}".format(self.totTimePct, self.selfTime, self.totTime, self.numCalls))
+      print("  stats: tot%:{0} self:{1} tot:{2} calls:{3}".format(
+         self.totTimePct, self.selfTime, self.totTime, self.numCalls))
       print("  depth: {0}".format(self.getMinDepth()))
       print("  callers:")
       for e in self.callerEdges:
@@ -90,6 +127,8 @@ class Node(object):
 # - will eventually keep more stats
 #
 class Edge(object):
+   edgeID = 1
+   # constructor
    def __init__(self,cg,callerId,calleeId,numCalls,totCalls):
       self.cg = cg
       self.callee = cg.nodeTable[calleeId]
@@ -106,53 +145,67 @@ class Edge(object):
       if self.callee.numCalls != totCalls:
          print("Error: callee numCalls not equal to totCalls: {0} {1}".
                       format(self.callee.numCalls,totCalls))
+      self.id = Edge.edgeID
+      Edge.edgeID = Edge.edgeID + 1
+      cg.edgeTable[self.id] = self
    # print method
    def printMe(self,space):
-      print("{0}from {1}:{4} to {2}:{5} count {3}".format(space, self.caller.id, self.callee.id, self.numCalls, self.caller.name, self.callee.name))
+      print("{0}from {1}:{4} to {2}:{5} count {3}".format(space,
+         self.caller.id, self.callee.id, self.numCalls, self.caller.name,
+         self.callee.name))
 
 #
 # Read gprof table and create call graph objects
 # param filename is the filename that the gprof output exists in
 #
 def createProfileGraph(filename):
-   inCGTable = False
-   inFlatTable = False
+   inCGTable = False    # true when we are processing DG data lines
+   inFlatTable = False  # true when we are processing flat data lines
    inf = open(filename)
+   # TODO error handling here
    cgraph = CallGraph()
    while True:
       line = inf.readline()
       if inFlatTable:
+         # we leave the flat profile table when we see an empty line
          if len(line) < 2:
             inFlatTable = False
             continue
+         # else process the data line
          processFlatProfileLine(line, cgraph)
       if inCGTable:
+         # we leave the flat profile table when we see an empty line
          if len(line) < 2:
             inCGTable = False
-            break
+            break  # leave the entire loop, since rest of file is not used
+         # else process the data line
          processCallGraphSection(line, inf, cgraph)
+      # header of flat profile signals we are in it
       if line.find("time   seconds   seconds    calls") > 0:
          inFlatTable = True
          continue
+      # header of call graph profile signals we are in it
       if None != re.match("index\s+%\s+time\s+self\s+children\s+called",line):
          inCGTable = True
-         print("In CG Table")
+         if debug: print("In CG Table")
    return cgraph
 
 #
 # Process a call graph section
-# - incoming line is first line in section
+# - incoming line is first line in section (a caller line)
+# - sections have caller lines, the function, then callees
 #
 def processCallGraphSection(line, fileh, cgraph):
    haveCaller = False
    haveFunction = False
    needLine = False
-   #print("CG first line: {0}".format(line), end='')
+   if debug: print("CG first line: {0}".format(line), end='')
+   # a horizontal line separator indicates end of section
    while line.find("------------") < 0:
       if needLine:
          line = fileh.readline()
          needLine = False
-      #print("CG processing line: {0}".format(line), end='')
+      if debug: print("CG processing line: {0}".format(line), end='')
       # First line in section is the callee, or "<spontaneous>"
       if not haveCaller:
          # match line like "                   <spontaneous>"
@@ -172,8 +225,9 @@ def processCallGraphSection(line, fileh, cgraph):
             callerChildTime = float(v.group(2))
             callerNumCalls = int(v.group(3))
             callerTotCalls = int(v.group(4))
-         print("Caller: {0} {1} {2} {3} {4} {5}".format(callerName, callerId,
-             callerSelfTime, callerChildTime, callerNumCalls, callerTotCalls))
+         if debug: print("Caller: {0} {1} {2} {3} {4} {5}".format(callerName,
+              callerId, callerSelfTime, callerChildTime, callerNumCalls,
+              callerTotCalls))
          haveCaller = True
          needLine = True
          continue
@@ -198,8 +252,9 @@ def processCallGraphSection(line, fileh, cgraph):
             funcSelfTime = float(v.group(3))
             funcChildrenTime = float(v.group(4))
             funcNumCalls = 0
-         print("Function: {0} {1} {2} {3} {4} {5}".format(funcName, funcId, 
-                 funcTotTimePct, funcSelfTime, funcChildrenTime, funcNumCalls))
+         if debug: print("Function: {0} {1} {2} {3} {4} {5}".format(funcName, 
+                  funcId, funcTotTimePct, funcSelfTime, funcChildrenTime,
+                  funcNumCalls))
          haveFunction = True
          needLine = True
          continue
@@ -215,29 +270,36 @@ def processCallGraphSection(line, fileh, cgraph):
          childChildrenTime = float(v.group(2))
          childNumCalls = int(v.group(3))
          childTotCalls = int(v.group(4))
-         print("Child: {0} {1} {2} {3} {4} {5}".format(childName, childId, 
-               childSelfTime, childChildrenTime, childNumCalls, childTotCalls))
+         if debug: print("Child: {0} {1} {2} {3} {4} {5}".format(childName, 
+             childId, childSelfTime, childChildrenTime, childNumCalls,
+             childTotCalls))
          needLine = True
       else:  # must be end of section
-         print("do end of section")
+         if debug: print("do end of section")
          # for now, ignore child info (not saved anyways)
          haveCaller = False
          haveFunction = False
-         if not callerId in Node.nodeTable:
+         if not callerId in cgraph.nodeTable:
             n = Node(cgraph,callerName,callerId,0,0,0,0)
-         if not funcId in Node.nodeTable:
-            n = Node(cgraph,funcName,funcId, funcTotTimePct, funcSelfTime, funcChildrenTime, funcNumCalls)
+         if not funcId in cgraph.nodeTable:
+            n = Node(cgraph,funcName,funcId, funcTotTimePct, funcSelfTime,
+                     funcChildrenTime, funcNumCalls)
          else:
-            n = Node.nodeTable[funcId]
-            n.update(funcTotTimePct, funcSelfTime, funcChildrenTime, funcNumCalls)
+            n = cgraph.nodeTable[funcId]
+            n.update(funcTotTimePct, funcSelfTime, funcChildrenTime,
+                     funcNumCalls)
          if funcName in cgraph.flatProfileData:
             n.updateFlatData(cgraph.flatProfileData)
          c = Edge(cgraph,callerId,funcId,callerNumCalls,callerTotCalls)
          # do not set needLine since we'll return to caller after this
 
+#
+# Process one line in the flat profile data section
+# - lines are either full data lines or partial (short) data lines
+#
 def processFlatProfileLine(line, cgraph):
    # line is either a full data line or a "short" line
-   print("flat line: {0}".format(line),end="")
+   if debug: print("flat line: {0}".format(line),end="")
    # try to match full line first
    v = re.match("\s*(\d+\.\d+)\s*(\d+\.\d+)\s*(\d+\.\d+)\s*(\d+)\s*(\d+\.\d+)\s*(\d+\.\d+)\s*([^\n\r]*)", line)
    short = 0
@@ -264,14 +326,62 @@ def processFlatProfileLine(line, cgraph):
    cgraph.flatProfileData[funcName] = (fpct,fstime,fcalls)
    return True
 
-#
+#---------------------------------------------------------------------
 # Main
-#
-cgraph = createProfileGraph(sys.argv[1])      
+#---------------------------------------------------------------------
+argParser = argparse.ArgumentParser(description='Gprof data manipulator')
+argParser.add_argument('--debug', action='store_true', help='turn on debugging info (default off)')
+argParser.add_argument('--dot', action='store_true', help='output Dot graph format (default: off)')
+argParser.add_argument('--text', action='store', default='gprof.txt', metavar='<gprof-report-file>', help='filename of text gprof report')
+argParser.add_argument('--bin', action='store', nargs=2, metavar='<filename>', help='invoke gprof on <exectuable gmon.out> pair')
+argParser.add_argument('--bindir', action='store', nargs=2, metavar='<name>', help='invoke gprof on all profiles in <exectuable directory> pair')
+args = argParser.parse_args()
+debug = args.debug
+doDot = args.dot
+textFile = args.text
 
-for n in cgraph.nodeTable:
-   node = cgraph.nodeTable[n]
-   node.printMe()
+if args.bindir is None:
+   #
+   # Single profile data file
+   #
+   if args.bin is not None:
+      if debug: print("do gprof report generation")
+      textFile = "{0}.txt".format(args.bin[1])
+      os.system("gprof {0} {1} > {2}".format( args.bin[0], args.bin[1],
+                textFile))
+
+   cgraph = createProfileGraph(textFile)
+
+   if doDot:
+      cgraph.outputDot()
+      exit(0)
+
+   # default action: print nodes in call graph
+   for n in cgraph.nodeTable:
+      node = cgraph.nodeTable[n]
+      node.printMe()
+   exit(0)
+
+else:
+   #
+   # Directory full of profile data (binary)
+   #
+   sd = os.scandir(args.bindir[1])
+   for f in sd:
+      if not f.is_file(): continue
+      proFile = "{0}/{1}".format(args.bindir[1],f.name)
+      textFile = "{0}.txt".format(proFile)
+      if debug: print("do gprof report generation ({0})".format(proFile))
+      os.system("gprof {0} {1} > {2}".format(args.bindir[0], proFile, 
+                textFile))
+      cgraph = createProfileGraph(textFile)
+      os.system("rm -f {0}".format(textFile))
+      cgraph.outputLibSVMLine()
+      # default action: print nodes in call graph
+      #for n in cgraph.nodeTable:
+      #   node = cgraph.nodeTable[n]
+      #   node.printMe()
 
 exit(0)
+
 
